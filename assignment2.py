5b3015bc8f6b2cbd59f68a9ce7bcd0a251c58b8b
michael-j-jones <git@mjones.xyz>
2018-02-07 18:20:08 -0500

    Added weighting for the 4 measurements, adding FAR test

--- assignment2.py
+++ assignment2.py
@@ -3,12 +3,16 @@ import pandas as pd
 
 # Measurements are labeled as A, B, C, D
 measurementID = ['A', 'B', 'C', 'D']
+a_weight = 1
+b_weight = 0.25
+c_weight = 0.8
+d_weight = 0.5
 pd.options.mode.chained_assignment = None
 
 def lowest_percentage_match(traindata, testdata):
     percentagedf = pd.DataFrame()
     percentagedf['TrueID'] = testdata['ID']
-    percentagedf['PercentSumOff'] = 100000
+    percentagedf['PercentSumOff'] = 100000.
     percentagedf['PredictedID'] = ''
     for k in range(len(testdata['ID'])):
         saved_pct = 1000
@@ -18,17 +22,28 @@ def lowest_percentage_match(traindata, testdata):
             b_pct = np.abs(testdata.B[k] - traindata.B[i]) / testdata.B[k]
             c_pct = np.abs(testdata.C[k] - traindata.C[i]) / testdata.C[k]
             d_pct = np.abs(testdata.D[k] - traindata.D[i]) / testdata.D[k]
-            sum_pct = a_pct + b_pct + c_pct + d_pct
+            sum_pct = a_pct*a_weight + b_pct*b_weight + c_pct*c_weight + d_pct*d_weight
 
             if sum_pct < saved_pct:
                 saved_ID = traindata['ID'][i]
                 saved_pct = sum_pct
-
         percentagedf.PercentSumOff[k] = saved_pct
         percentagedf.PredictedID[k] = saved_ID
 
+
     return percentagedf
 
+def FARtest(trueID, predictedID):
+    tp = sum(trueID == predictedID)
+    tn = 0
+    fp = len(trueID) - tp
+    fn = len(trueID) - tn
+
+    acc = (tp + tn) / (tp + fp + tn + fn)
+    err = (fp + fn) / (tp + fp + tn + fn)
+
+    print('tp = ', tp)
+    print('Accuracy: ', acc, ', Error: ', err)
 
 
 if __name__ == '__main__':
@@ -37,5 +52,5 @@ if __name__ == '__main__':
     # Testing data, sample 2
     testdf = pd.read_csv('test.csv')
 
-
     percentagedf = lowest_percentage_match(traindf, testdf)
+    FARtest(percentagedf['TrueID'], percentagedf['PredictedID'])
